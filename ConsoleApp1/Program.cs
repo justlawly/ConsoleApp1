ee15b1d964c17fde9a28fddf2dec629e4d85af29
WADU <jingroel@gmail.com>
2018-10-29 14:02:42 +0200

    lol

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("omega LUL");
-            Console.WriteLine("wadu wadu wadu wadu LUL");
+            Console.WriteLine("math");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("eng");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("phyz");
+            double c = double.Parse(Console.ReadLine());
+            double d;
+            double dd;
+            a = a * 0.4;
+            b = b * 0.2;
+            c = c * 0.4;
+            d = (a + b + c) / 3;
+            
+            Console.WriteLine("sum is="+d);
+            Console.WriteLine("oreh");
+            double e = double.Parse(Console.ReadLine());
+            Console.WriteLine("rohav");
+            double f = double.Parse(Console.ReadLine());
+            d = 2*(e+f);
+            dd = e * f;
+            Console.WriteLine("hekef is= "+d+"shetah is "+dd);
+
+            int i = 1;
+            int sum = 1;
+            Console.WriteLine("enter number");
+            int n = int.Parse(Console.ReadLine());
+            while(i<=n)
+            {
+                sum *=i;
+                i++;
+            }
+            Console.WriteLine("blabla is= " + sum);
+
+
+
+
         }
     }
 }
